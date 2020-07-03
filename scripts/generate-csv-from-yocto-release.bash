9cee39a1c56d3657d1ec3b2c7466614fe93ee3f2
Nathan Genetzky <nathan@genetzky.us>
2020-07-03 11:58:18 -0500

    scripts: Add usage help for generate-csv-from-yocto-release

--- scripts/generate-csv-from-yocto-release.bash
+++ scripts/generate-csv-from-yocto-release.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Usage Examples:
 #
-# ./scripts/generate-csv-from-yocto-release.bash 'poky-glibc-x86_64-core-image-sato-aarch64-qemuarm64' > data/toolchain-x86_64-aarch64-qemuarm64.csv
 
 # Bash Strict Mode # For scripts that already depend on Bash.
 set -eu -o pipefail
@@ -10,6 +9,14 @@ IFS=$'\n\t'
 D_SCRIPT="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
 D_BASE="${D_BASE-"${D_SCRIPT}/../"}"
 DEFAULT_VERSION='3.1.1'
+
+_usage(){
+    _echoerr "Usage: $0 grep_arg [version]"
+    _echoerr ""
+    _echoerr "Usage Examples"
+    _echoerr "$0 'poky-glibc-x86_64-core-image-sato-aarch64-qemuarm64' > 'data/toolchain-all-x86_64-aarch64-qemuarm64.csv'"
+    _echoerr "$0 'poky-glibc-x86_64-core-image-sato-aarch64-qemuarm64.*toolchain-[[:digit:]]' > 'data/toolchain-x86_64-aarch64-qemuarm64.csv'"
+}
 
 generate_csv_from_yocto_release(){
     local grep_arg
@@ -34,5 +41,14 @@ generate_csv_from_yocto_release(){
         | cut -f 2 - -d ' ' \
         | sed -e "s#^#${version},#"
 }
+
+_echoerr(){
+	echo "$*" >&2
+}
+
+if [ "$#" -eq 0 ]; then
+    _usage
+    exit 2
+fi
 
 generate_csv_from_yocto_release "$@"
